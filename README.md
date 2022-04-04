9d0e2c00a6d710fff855fad1e52999b6f9cbb539
1804011 <sharifsharifulislam061@gmail.com>
2022-04-04 20:39:14 +0600

    assignment9 done

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Website Link: [Laptop Review Website ](https://sharif-assignment9.netlify.app).
 ### Task completed in this project
 
 - Home Page include Banner & Customer Review
-- Review section includes All reveiws of customer
+- Review section includes All reviews of customer
 - Dashboard includes two charts
 - Blogs Includes Questions & Answer.
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./About.css";
 const About = () => {
-	return <div>this is about</div>;
+	return (
+		<div className="about-container">
+			<h1 className="text-2xl font-bold italic w">
+				This projects intend to practise react routing. So to be familiarize
+				with react routing we build a laptop review site by using react router.
+			</h1>
+		</div>
+	);
 };
 
 export default About;
